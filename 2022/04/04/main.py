3a895962baf3bc3916dba393b3592c9f10367e66
Nicholas Villarreal <nvillarreal@protonmail.com>
2022-12-02 23:59:32 -0700

    Prep day 4

---
+++ 2022/04/04/main.py
@@ -0,0 +1,16 @@
+def puzzle_one():
+  total = 0
+  for line in open("input.txt"):
+    print(line)
+
+  print(f"Part 1: {total}")
+
+puzzle_one()
+
+# def puzzle_two():
+#   total = 0
+#   for line in open("input.txt"):
+#     print(line)
+#   print(f"Part 2: {total}")
+
+# puzzle_two()
