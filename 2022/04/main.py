fe7e960cf7d1c1e30df1990fca5e37553048a346
Nicholas Villarreal <nvillarreal@protonmail.com>
2022-12-04 15:21:37 -0700

    Solve day 4

--- 2022/04/main.py
+++ 2022/04/main.py
@@ -1,8 +1,13 @@
-
 def puzzle_one():
   total = 0
   for line in open("input.txt"):
-    print(line)
+    pairs = line.strip().split(',')
+    x1, x2 = [int(id) for id in pairs[0].split('-')]
+    y1, y2 = [int(id) for id in pairs[1].split('-')]
+
+    # Test if either pair of numbers contains the other
+    if (x1 >= y1 and x2 <= y2) or (y1 >= x1 and y2 <= x2):
+      total +=1
 
   print(f"Part 1: {total}")
 
@@ -11,7 +16,14 @@ puzzle_one()
 def puzzle_two():
   total = 0
   for line in open("input.txt"):
-    print(line)
+    pairs = line.strip().split(',')
+    x1, x2 = [int(id) for id in pairs[0].split('-')]
+    y1, y2 = [int(id) for id in pairs[1].split('-')]
+
+    # Intersection
+    if x1 <= y2 and y1 <= x2:
+      total += 1
+
   print(f"Part 2: {total}")
 
 puzzle_two()
