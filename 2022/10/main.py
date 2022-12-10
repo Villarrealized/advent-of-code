ef42f3d8465f5af5b804364e6a2f80773fc68801
Nicholas Villarreal <nvillarreal@protonmail.com>
2022-12-10 02:03:14 -0700

    Add day 10

--- 2022/10/main.py
+++ 2022/10/main.py
@@ -1,8 +1,26 @@
 def puzzle(part_two=False):
-    total = 0
+    cycles = []
+    cycle = 0
+    x = 1
     for line in open("input.txt"):
-        print(line)
-        print(f"Part 1: {total}")
+        command = line.split()
+        if command[0] == "noop":
+            cycle += 1
+            cycles.append(x)
+            continue
+        else:
+            for _ in range(0, 2):
+                cycle += 1
+                cycles.append(x)
+
+            x += int(command[1])
+
+    total = 0
+    for index in range(19, len(cycles), 40):
+        total += (index + 1) * cycles[index]
+    # print(signal_strengths)
+    # print(sum(signal_strengths))
+    print(f"Part 1: {total}")
 
 
 puzzle()
