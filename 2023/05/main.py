c35c20b0d549831405b45ca10caff83131cd1ece
Nicholas Villarreal <nvillarreal@protonmail.com>
2023-12-06 01:40:05 -0700

    Update day 5

--- 2023/05/main.py
+++ 2023/05/main.py
@@ -1,43 +1,46 @@
-seeds = []
-maps = []
+# seeds = []
+# maps = []
 
-map = []
-for index, line in enumerate(open("input.txt")):
-    if index == 0:
-        name, values = line.split(":")
-        seeds = [int(value) for value in values.strip().split()]
-    else:
-        if line[0].isalpha():
-            if len(map):
-                maps.append(map)
-            map = []
-        elif line[0].isdigit():
-            map.append([int(value) for value in line.strip().split()])
+# map = []
+# for index, line in enumerate(open("input.txt")):
+#     if index == 0:
+#         name, values = line.split(":")
+#         seeds = [int(value) for value in values.strip().split()]
+#     else:
+#         if line[0].isalpha():
+#             if len(map):
+#                 maps.append(map)
+#             map = []
+#         elif line[0].isdigit():
+#             map.append([int(value) for value in line.strip().split()])
 
-# Add the last map
-maps.append(map)
+# # Add the last map
+# maps.append(map)
 
-locations = []
-for seed in seeds:
-    val = seed
-    for map in maps:
-        for row in map:
-            dest, source, count = row
-            offset = dest - source
-            # print(offset)
-            if val in range(source, source + count):
-                val += offset
-                break
+# locations = []
+# for seed in seeds:
+#     val = seed
+#     for map in maps:
+#         for row in map:
+#             dest, source, count = row
+#             offset = dest - source
+#             # print(offset)
+#             if val in range(source, source + count):
+#                 val += offset
+#                 break
 
-    locations.append(val)
+#     locations.append(val)
 
-print(min(locations))
+# print(min(locations))
 
+from multiprocessing import Pool
+import tqdm
 
 # part 2
+import tqdm
+
 seeds = []
 maps = []
-
 map = []
 for index, line in enumerate(open("input.txt")):
     if index == 0:
@@ -58,19 +61,26 @@ for index, line in enumerate(open("input.txt")):
 # Add the last map
 maps.append(map)
 
-locations = []
-for index, seed in enumerate(seeds):
-    if index in [0, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000, 2_000_000_000]:
-        print(f"Processing seed {index + 1} of {len(seeds)}")
+
+def process(seed):
     val = seed
+    count = 1
     for map in maps:
         for row in map:
             dest, source, count = row
             offset = dest - source
             if val in range(source, source + count):
                 val += offset
+                count += 1
                 break
 
-    locations.append(val)
+    return val
 
-print(min(locations))
+
+if __name__ == "__main__":
+    length = len(seeds)
+    print(f"begin processing {length} seeds")
+    pool = Pool()
+    values = set(tqdm.tqdm(pool.imap_unordered(process, seeds), total=length))
+
+    print(min(values))
