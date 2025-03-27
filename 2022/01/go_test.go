3f98e393522183737b9fe9360258ad75547e1271
Nicholas Villarreal <nvillarreal@protonmail.com>
2025-03-27 15:36:08 -0600

    part 1 for go

--- 2022/01/go_test.go
+++ 2022/01/go_test.go
@@ -34,7 +34,24 @@ func TestPart1(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if have := part1(tt.input); have != tt.want {
-				t.Errorf("wrong output\nhave: %v, \nwant: %v", have, tt.want)
+				t.Errorf("wrong output\nhave: %v \nwant: %v", have, tt.want)
+			}
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	tests := []Test{
+		{
+			name:  "example",
+			input: example,
+			want:  45000,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if have := part2(tt.input); have != tt.want {
+				t.Errorf("wrong output\nhave: %v \nwant: %v", have, tt.want)
 			}
 		})
 	}
--- 2022/01/main.go
+++ 2022/01/main.go
@@ -1,11 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readInput() string {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	return strings.TrimRight(string(data), "\n")
+}
+
+func parseInput(input string) (output [][]int) {
+	for elf := range strings.SplitSeq(input, "\n\n") {
+		bag := []int{}
+		for food := range strings.SplitSeq(elf, "\n") {
+			calories, err := strconv.Atoi(food)
+			if err != nil {
+				panic(err)
+			}
+			bag = append(bag, calories)
+		}
+		output = append(output, bag)
+	}
+	return
+}
 
 func main() {
-	fmt.Println("Hello world")
+	input := readInput()
+	result := part1(input)
+
+	fmt.Printf("part 1: %v\n", result)
 }
 
 func part1(input string) int {
+	elves := parseInput(input)
+
+	max_cals := 0
+
+	for _, elf := range elves {
+		sum := 0
+		for _, calories := range elf {
+			sum += calories
+		}
+		if sum > max_cals {
+			max_cals = sum
+		}
+	}
+
+	return max_cals
+}
+
+func part2(input string) int {
+	// elves := parseInput(input)
+
 	return 0
 }
